e3e6e1f52f38ad2ddfd1e76f4f56696fb124153d
rumyantsevat <ipochinili@gmail.com>
2019-09-15 23:05:24 +0300

    route ArticleController

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get ('/', 'HomeController@index');
 Route::get ('about', 'PagesController@about');
 Route::get ('contact', 'PagesController@contact');
+Route::get('articles', 'ArticlesController@index');
