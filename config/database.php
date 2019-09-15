78574a444368f1adf1628db98215597ad9fbf60b
rumyantsevat <ipochinili@gmail.com>
2019-09-15 21:00:16 +0300

    Migrations

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => true,// change false  to true
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
