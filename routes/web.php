593886a6881b7e13776699dcdcbf53aeb3b14da7
rumyantsevat <ipochinili@gmail.com>
2019-09-15 16:45:22 +0300

    page about

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,7 @@
 |
 */
 
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('pages.about');
-});
-
-Route::get('/contact', function () {
-    return view('pages.contact');
-});
+Route::get ('about', 'PagesController@about');
