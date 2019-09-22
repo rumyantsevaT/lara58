cc5d9bc69ad0240771933eaab6fe52a2023ab42f
rumyantsevat <ipochinili@gmail.com>
2019-09-22 20:13:38 +0300

    Eloquent

--- app/Article.php
+++ app/Article.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+//    поля для массового заполнения при Eloquent create()
+//это атрибуты, которые мы ПОЗВОЛЯЕМ массово прописывать
     protected $fillable = [
         'title',
         'body',
